8ed63d7a610c242f7b48152e58c7497a795253ef
gygy2006 <gygy2006@naver.com>
2021-05-18 18:35:24 +0900

    init

---
+++ core/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class TimestampedModel(models.Model):
+    datetime_update = models.DateTimeField(auto_now=True)
+    datetime_created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        abstract = True
+        ordering = ['-datetime_update', '-datetime_created']
