69e11834fb81e692511b88d4ff3afbae268a4970
gygy2006 <gygy2006@naver.com>
2021-05-19 19:52:17 +0900

    2105291952

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,7 @@
-import "./App.css";
 import { Provider } from "react-redux";
 import store, { persistor } from "./redux/store";
 import { Route, BrowserRouter } from "react-router-dom";
 import { PersistGate } from "redux-persist/integration/react";
-import SignInContainer from "./routes/Auth/SignIn/SignInContainer";
 import Home from "./routes/Home";
 
 function App() {
--- frontend/src/routes/Auth/SignIn/SignInContainer.js
+++ frontend/src/routes/Auth/SignIn/SignInContainer.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { userLogin } from "../../../redux/userSlice";
-
+import { logOut, userLogin } from "../../../redux/userSlice";
+import "../../../style/scss/SignIn.sass";
 export default (props) => {
   const { isLoggedIn } = useSelector((state) => state.usersReducer);
   const dispatch = useDispatch();
@@ -13,7 +13,6 @@ export default (props) => {
   const onChangePassword = (e) => {
     setPassword(e.target.value);
   };
-  console.log(email, password);
   const handleSubmit = async () => {
     await dispatch(
       userLogin({
@@ -23,34 +22,33 @@ export default (props) => {
     );
     document.location = "http://localhost:3000/";
   };
-
+  const handleLogOut = async () => {
+    dispatch(logOut());
+  };
   return isLoggedIn ? (
-    ""
+    <div>
+      <button onClick={handleLogOut}>LogOut</button>
+    </div>
   ) : (
-    <div className="bg-grey-lighter min-h-200 flex flex-col">
-      <div className="container max-w-xl mx-auto flex-1 flex flex-col items-center justify-center px-2">
-        <div className="bg-white px-6 py-8 rounded shadow-md text-black w-full">
-          <h1 className="mb-8 text-3xl text-center">LogIn</h1>
+    <div>
+      <div>
+        <div>
+          <h1>LogIn</h1>
           <input
-            className="block border border-grey-light w-full p-3 rounded mb-4"
+            className="login-email"
             type="email"
             value={email}
             onChange={onChangeEmail}
             placeholder="Email"
           />
           <input
-            className="block border border-grey-light w-full p-3 rounded mb-4"
+            className="login-password"
             type="password"
             value={password}
             onChange={onChangePassword}
             placeholder="Password"
           />
-          <button
-            className="w-full text-center py-3 rounded bg-green-400 text-white hover:bg-green-dark focus:outline-none my-1"
-            onClick={handleSubmit}
-          >
-            Login
-          </button>
+          <button onClick={handleSubmit}>Login</button>
         </div>
       </div>
     </div>
