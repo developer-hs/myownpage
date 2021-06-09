ea80a1e148a815617e39b6670e6acf152bfff840
gygy2006 <gygy2006@naver.com>
2021-06-10 00:17:19 +0900

    2106100017

--- backend/notepad/models.py
+++ backend/notepad/models.py
@@ -4,3 +4,4 @@ from core.models import models
 class NotePads(TimestampedModel):
     memo = models.TextField(blank=True)
     user = models.ForeignKey("users.User" , related_name="notepads" ,on_delete=models.CASCADE)
+    done = models.BooleanField(default=False)
--- backend/notepad/serializers.py
+++ backend/notepad/serializers.py
@@ -1,11 +1,12 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, BooleanField
 from users.serializers import UserSerializer
 from .views import NotePads
 
 
 class NotePadSerializer(ModelSerializer):
+    done = BooleanField(read_only=True)
 
     class Meta:
         model = NotePads
-        fields = ("id", "memo", "datetime_created", "datetime_update")
+        fields = ("id", "memo", "done", "datetime_created", "datetime_update")
         read_only_fields = ("id", "datetime_created", "datetime_update")
