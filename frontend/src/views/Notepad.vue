b87e89d86f01fcad33ea41ffe50e31b2e320dbdc
gygy2006 <gygy2006@naver.com>
2021-06-12 21:50:27 +0900

    2106122150

--- frontend/src/views/Notepad.vue
+++ frontend/src/views/Notepad.vue
@@ -1,7 +1,7 @@
 <template>
-  <v-card flat class="mt-16 rounded-0" max-width="600">
+  <v-card height="600" class="pt-3 mt-16 rounded-0 pa-3" max-width="600">
     <v-row align="center" justify="center">
-      <v-row class="pl-4">
+      <v-row class="pt-3 pl-5">
         <v-col cols="auto">
           <v-icon color="red" small>mdi-book-open-outline</v-icon>
           <span class="text-body-2 red--text" :key="`tasks-${notepad.length}`">
@@ -24,70 +24,82 @@
         <v-text-field
           dense
           v-model="memoText"
-          label="메모를 등록해 주세요"
+          label="할 일을 등록해 주세요"
           solo
           @keypress.enter="createMemo"
         >
         </v-text-field>
       </v-col>
     </v-row>
+    <v-col v-if="notepad.length > 0" class="justify-space-between">
+      <v-card flat height="400">
+        <v-scroll-y-reverse-transition hide-on-leave group>
+          <template v-for="(note, index) in notepad">
+            <v-divider v-if="index !== 0" :key="`${index}-divider`"></v-divider>
 
-    <v-card flat v-if="notepad.length > 0">
-      <v-scroll-y-reverse-transition hide-on-leave group>
-        <template v-for="(note, index) in notepad">
-          <v-divider v-if="index !== 0" :key="`${index}-divider`"></v-divider>
-
-          <v-list-item :key="`${index}-${note.memo}`">
-            <v-list-item-action>
-              <v-checkbox
-                @click="updateMemo(note)"
-                v-model="note.done"
-                color="info darken-3"
-              >
-                <div
-                  slot="label"
-                  :class="(note.done && 'grey--text') || 'text--primary'"
-                  class="ml-3"
-                  v-text="note.memo"
-                ></div>
-              </v-checkbox>
-            </v-list-item-action>
-
-            <v-spacer></v-spacer>
-
-            <v-scroll-x-transition>
-              <div v-if="!note.done">
-                <v-btn
-                  @click="removeMemo(notepad[index].id)"
-                  class="mr-3"
-                  x-small
-                  icon
-                  fab
-                  depressed
-                  ><v-icon color="red">mdi-delete-empty</v-icon></v-btn
+            <v-list-item :key="`${index}-${note.memo}`">
+              <v-list-item-action>
+                <v-checkbox
+                  @click="updateMemo(note)"
+                  v-model="note.done"
+                  color="info darken-3"
                 >
-                <v-btn
-                  class="mr-3"
-                  icon
-                  x-small
-                  fab
-                  depressed
-                  @click="
-                    dialog = !dialog;
-                    updateMemoText = note.memo;
-                    updateNote = note;
-                  "
-                  ><v-icon color="">mdi-pencil-plus</v-icon>
-                </v-btn>
-              </div>
-              <v-icon v-if="note.done" color="success">
-                mdi-check
-              </v-icon>
-            </v-scroll-x-transition>
-          </v-list-item>
-        </template>
-      </v-scroll-y-reverse-transition>
-    </v-card>
+                  <div
+                    slot="label"
+                    :class="(note.done && 'grey--text') || 'text--primary'"
+                    class="ml-3 text-subtitle-2"
+                    v-text="note.memo"
+                  ></div>
+                </v-checkbox>
+              </v-list-item-action>
+
+              <v-spacer></v-spacer>
+
+              <v-scroll-x-transition>
+                <div v-if="!note.done">
+                  <v-btn
+                    @click="removeMemo(notepad[index].id)"
+                    class="mr-3"
+                    x-small
+                    icon
+                    fab
+                    depressed
+                    ><v-icon color="red">mdi-delete-empty</v-icon></v-btn
+                  >
+                  <v-btn
+                    class="mr-3"
+                    icon
+                    x-small
+                    fab
+                    depressed
+                    @click="
+                      dialog = !dialog;
+                      updateMemoText = note.memo;
+                      updateNote = note;
+                    "
+                    ><v-icon color="">mdi-pencil-plus</v-icon>
+                  </v-btn>
+                </div>
+                <v-icon v-if="note.done" color="success">
+                  mdi-check
+                </v-icon>
+              </v-scroll-x-transition>
+            </v-list-item>
+          </template>
+        </v-scroll-y-reverse-transition>
+      </v-card>
+
+      <div class="text-center">
+        <v-pagination v-model="page" :length="4" circle></v-pagination>
+      </div>
+    </v-col>
+
+    <v-container v-else>
+      <v-col cols="12" class="text-center grey--text text-body-1">
+        Memo is Empty !!
+      </v-col>
+    </v-container>
+
     <!-- update -->
     <v-dialog v-model="dialog" max-width="70rem">
       <v-card class="pa-6">
@@ -126,6 +138,7 @@ export default {
           text: "Fizzbuzz"
         }
       ],
+      page: 1,
       task: null,
       group: true,
       memoText: "",
