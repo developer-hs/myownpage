69fd0a49428690149d1dc673ecdaa6d8f510ddf4
gygy2006 <gygy2006@naver.com>
2021-06-14 22:30:27 +0900

    2106142230

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,16 +1,21 @@
+import vuetify from "./plugins/vuetify";
+import { TiptapVuetifyPlugin } from "tiptap-vuetify";
+// don't forget to import CSS styles
+import "tiptap-vuetify/dist/main.css";
+// Vuetify's CSS styles
+import "vuetify/dist/vuetify.min.css";
 import Vue from "vue";
 import App from "./App.vue";
 import store from "./store";
-import vuetify from "./plugins/vuetify";
 import router from "./router";
 
 Vue.config.productionTip = false;
-
+Vue.use(TiptapVuetifyPlugin, { vuetify, iconsGroup: "mdi" });
 export const eventBus = new Vue();
 
 new Vue({
   router,
   store,
   vuetify,
-  render: (h) => h(App),
+  render: h => h(App)
 }).$mount("#app");
--- frontend/src/views/NotePad/MemoType.vue
+++ frontend/src/views/NotePad/MemoType.vue
@@ -1,43 +1,195 @@
 <template>
-  <v-card rounded="lg" elevation="1" height="600" class="pa-3" max-width="600">
-    <v-btn @click="dialog = true" color="pink" fab dark small absolute top left>
-      <v-icon>mdi-plus</v-icon>
-    </v-btn>
-    <v-row>
-      <v-col cols="6" :key="i" v-for="i in 3">
-        <v-card height="230px"
-          ><v-img
-            lazy-src="https://picsum.photos/id/11/10/6"
-            height="230"
-            max-width="248.25"
-            src="https://picsum.photos/id/11/500/300"
-          >
-          </v-img
-        ></v-card>
-      </v-col>
-    </v-row>
-    <v-dialog v-model="dialog" max-width="70rem">
-      <v-card class="pa-6">
-        <v-card-text>
-          <span>메모 등록</span>
-          <v-textarea auto-grow counter v-model="text"></v-textarea>
-        </v-card-text>
-        <v-row justify="end">
-          <v-btn text color="primary" @click="dialog = false">
-            Submit
-          </v-btn>
-        </v-row>
-      </v-card>
-    </v-dialog>
-  </v-card>
+  <div>
+    <bubble-menu
+      class="bubble-menu"
+      :tippy-options="{ duration: 100 }"
+      :editor="editor"
+      v-if="editor"
+    >
+      <button
+        @click="
+          editor
+            .chain()
+            .focus()
+            .toggleBold()
+            .run()
+        "
+        :class="{ 'is-active': editor.isActive('bold') }"
+      >
+        Bold
+      </button>
+      <button
+        @click="
+          editor
+            .chain()
+            .focus()
+            .toggleItalic()
+            .run()
+        "
+        :class="{ 'is-active': editor.isActive('italic') }"
+      >
+        Italic
+      </button>
+      <button
+        @click="
+          editor
+            .chain()
+            .focus()
+            .toggleStrike()
+            .run()
+        "
+        :class="{ 'is-active': editor.isActive('strike') }"
+      >
+        Strike
+      </button>
+    </bubble-menu>
+
+    <floating-menu
+      class="floating-menu"
+      :tippy-options="{ duration: 100 }"
+      :editor="editor"
+      v-if="editor"
+    >
+      <button
+        @click="
+          editor
+            .chain()
+            .focus()
+            .toggleHeading({ level: 1 })
+            .run()
+        "
+        :class="{ 'is-active': editor.isActive('heading', { level: 1 }) }"
+      >
+        H1
+      </button>
+      <button
+        @click="
+          editor
+            .chain()
+            .focus()
+            .toggleHeading({ level: 2 })
+            .run()
+        "
+        :class="{ 'is-active': editor.isActive('heading', { level: 2 }) }"
+      >
+        H2
+      </button>
+      <button
+        @click="
+          editor
+            .chain()
+            .focus()
+            .toggleBulletList()
+            .run()
+        "
+        :class="{ 'is-active': editor.isActive('bulletList') }"
+      >
+        Bullet List
+      </button>
+    </floating-menu>
+
+    <editor-content :editor="editor" />
+  </div>
 </template>
 <script>
+import {
+  Editor,
+  EditorContent,
+  BubbleMenu,
+  FloatingMenu
+} from "tiptap-vuetify";
+import StarterKit from "@tiptap/starter-kit";
 export default {
+  components: {
+    EditorContent,
+    BubbleMenu,
+    FloatingMenu
+  },
+
   data() {
     return {
-      text: "",
-      dialog: false
+      editor: null
     };
+  },
+
+  mounted() {
+    this.editor = new Editor({
+      extensions: [StarterKit],
+      content: `
+        <p>
+          Try to select <em>this text</em> to see what we call the bubble menu.
+        </p>
+        <p>
+          Neat, isn’t it? Add an empty paragraph to see the floating menu.
+        </p>
+      `
+    });
+  },
+
+  beforeDestroy() {
+    this.editor.destroy();
   }
 };
 </script>
+
+<style lang="scss">
+/* Basic editor styles */
+.ProseMirror {
+  > * + * {
+    margin-top: 0.75em;
+  }
+
+  ul,
+  ol {
+    padding: 0 1rem;
+  }
+
+  blockquote {
+    padding-left: 1rem;
+    border-left: 2px solid rgba(#0d0d0d, 0.1);
+  }
+}
+
+.bubble-menu {
+  display: flex;
+  background-color: #0d0d0d;
+  padding: 0.2rem;
+  border-radius: 0.5rem;
+
+  button {
+    border: none;
+    background: none;
+    color: #fff;
+    font-size: 0.85rem;
+    font-weight: 500;
+    padding: 0 0.2rem;
+    opacity: 0.6;
+
+    &:hover,
+    &.is-active {
+      opacity: 1;
+    }
+  }
+}
+
+.floating-menu {
+  display: flex;
+  background-color: #0d0d0d10;
+  padding: 0.2rem;
+  border-radius: 0.5rem;
+
+  button {
+    border: none;
+    background: none;
+    font-size: 0.85rem;
+    font-weight: 500;
+    padding: 0 0.2rem;
+    opacity: 0.6;
+
+    &:hover,
+    &.is-active {
+      opacity: 1;
+    }
+  }
+}
+</style>
